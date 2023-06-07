85143f26ff41e8377749f8e1ba6f4009f5c603b9
Lewi-5 <124799335+Lewi-5@users.noreply.github.com>
2023-06-07 17:36:10 -0400

    latest test db update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,18 +1,17 @@
-module.exports = {
-    HOST: "fsd08workright.mysql.database.azure.com",
-    USER: "workright01",
-    PASSWORD: "FYxskC0aeL1pjh9t",
-    DB: "workrightdb"
-  };
-
-//Lewis' test database
 // module.exports = {
-//     HOST: "fsd08lewis.mysql.database.azure.com",
-//     USER: "dbAdmin",
-//     PASSWORD: "0W8r*7@3DHgA",
-//     DB: "workrighttest" // do not make typos on database names, disaster
+//     HOST: "fsd08workright.mysql.database.azure.com",
+//     USER: "workright01",
+//     PASSWORD: "FYxskC0aeL1pjh9t",
+//     DB: "workrightdb"
 //   };
 
+//Lewis' test database
+module.exports = {
+    HOST: "fsd08lewis.mysql.database.azure.com",
+    USER: "dbAdmin",
+    PASSWORD: "0W8r*7@3DHgA",
+    DB: "workrighttest" // do not make typos on database names, disaster
+  };
 
 
 // HOST: fsd08workright.mysql.database.azure.com
