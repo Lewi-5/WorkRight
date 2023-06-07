1d7da51206db0fd0b5b3f9dd20f7350eb1703307
JingWeiCandy <2343458@johnabbottcollege.net>
2023-06-07 11:24:47 -0400

    modidied api.js

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,24 +1,13 @@
-const express = require('express');
-const router = express.Router();
-const bodyParser = require('body-parser');
-router.use(bodyParser.text());
-
-const users = require("../controllers/user.controller.js");
-
-//TODO: how to put static files in router? use app.get in server for now
-// router
-//     .route('../addItem.html')
-//     .get()
-
-router
-    .route('/users')
-    .get(users.findAll)
-    .post(users.create) //mispelling enum got a data truncated message
-    
-router
-    .route('/users/:id([0-9]+)')
-    .get(users.findOne)
-    .patch(users.update)
-
-
-module.exports = router;
+module.exports = app => {
+    const auctions = require("../controllers/user.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new 
+    router.post("/", );
+  
+    // Retrieve all 
+    router.get("/", );
+  
+    app.use('/api', router);
+};
