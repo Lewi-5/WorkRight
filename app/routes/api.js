33c1f56d7f4c58cbe915534711c5358a25f37417
Lewi-5 <124799335+Lewi-5@users.noreply.github.com>
2023-06-07 02:38:58 -0400

    routes: postman tests on localhost:7077/api/users

---
+++ app/routes/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.text());
+
+const users = require("../controllers/user.controller.js");
+
+//TODO: how to put static files in router? use app.get in server for now
+// router
+//     .route('../addItem.html')
+//     .get()
+
+router
+    .route('/users')
+    .get(users.findAll)
+    .post(users.create) //mispelling enum got a data truncated message
+    
+router
+    .route('/users/:id([0-9]+)')
+    .get(users.findOne)
+    .patch(users.update)
+
+
+    module.exports = router;
