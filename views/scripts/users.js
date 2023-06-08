eb9293dfad9ac25a55877848d7b8705620c5a112
Lewi-5 <124799335+Lewi-5@users.noreply.github.com>
2023-06-08 09:30:50 -0400

    scrum

--- views/scripts/users.js
+++ views/scripts/users.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     $.get("http://localhost:7077/api/users", (data) => {
         for (let i = 0; i < data.length; i++) {
-            let tableRow = $("<tr></tr>");
+            let tableRow = $(`<tr class="row" id="row${i}"><div class="col-2"></div></tr>`); // not working yet to create cards of each row
       
             let idCell = $("<td></td>").text(data[i].ID);
             let usernameCell = $("<td></td>").text(data[i].username);
