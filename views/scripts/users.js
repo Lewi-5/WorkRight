5b742e71577d97ddfbe60cc9ba75b8dc0ea50d84
JingWeiCandy <2343458@johnabbottcollege.net>
2023-06-09 16:36:55 -0400

    commit

--- views/scripts/users.js
+++ views/scripts/users.js
@@ -1,4 +1,51 @@
 $(document).ready(function () {
+    
+    $('.nameWarning').hide();
+    $('.passwordWarning').hide();
+    $('.fnameWarning').hide();
+    $('.lnameWarning').hide();
+
+    $('#username').on('blur', function() {
+        console.log('Input lost focus');
+        let usernameInpt = $("#username").val();
+        if (usernameInpt == null || usernameInpt ==""){
+            $(".nameWarning").show();
+            return false;
+        }else{
+            $(".nameWarning").hide();
+            
+        }
+      });
+
+      $('#password').on('blur', function() {
+        let passwordInpt = $("#password").val();
+        if (passwordInpt == null || passwordInpt ==""){
+            $(".passwordWarning").show();
+            return false;
+        } else{
+            $(".passwordWarning").hide();
+        }
+      });
+
+      $('#firstName').on('blur', function() {
+        let firstNameInpt = $("#firstName").val();
+        if (firstNameInpt == null || firstNameInpt ==""){
+            $(".fnameWarning").show();
+            return false;
+        } else{
+            $(".fnameWarning").hide();
+        }
+      });
+
+      $('#lastName').on('blur', function() {
+        let lastNameInpt = $("#lastName").val();
+        if (lastNameInpt == null || lastNameInpt ==""){
+            $(".lnameWarning").show();
+            return false;
+        } else{
+            $(".lnameWarning").hide();
+        }
+      });
 
     const tableBody = $("#tableBody");
 
@@ -22,25 +69,50 @@ $(document).ready(function () {
       
       
       $(".addBtn").on("click", function(){
-        
             addNew();
+            refreshInputs();
         })
         
     
-    
-
+ 
 
     
 });
 
+function refreshInputs(){
+    $('.nameWarning').hide();
+    $('.passwordWarning').hide();
+    $('.fnameWarning').hide();
+    $('.lnameWarning').hide();
+    $("#username").val("");
+    $("#password").val("");
+    $("#firstName").val("");
+    $("#lastName").val("");
+}
 
       function addNew() {
         console.log("button clicked");
-        // let usernameInpt = $("#username").val();
-        // let passwordInpt = $("#password").val();
-        // let firstNameInpt = $("#firstName").val();
-        // let lastNameInpt = $("#lastName").val();
+         let usernameInpt = $("#username").val();
+         let passwordInpt = $("#password").val();
+         let firstNameInpt = $("#firstName").val();
+         let lastNameInpt = $("#lastName").val();
 
+         if (usernameInpt == null || usernameInpt ==""){
+            $(".nameWarning").show();
+            return false;
+        } 
+        if (passwordInpt == null || passwordInpt ==""){
+            $(".passwordWarning").show();
+            return false;
+        } 
+        if (firstNameInpt == null || firstNameInpt ==""){
+            $(".fnamedWarning").show();
+            return false;
+        } 
+        if (lastNameInpt == null || lastNameInpt ==""){
+            $(".lnameWarning").show();
+            return false;
+        } 
         let requestData = {
           username: $("#username").val(),
           password: $("#password").val(),
