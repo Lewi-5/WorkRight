19077c8e9ff43e38c1bd6e4f1297e48f00084827
Dustin Ruck <dustin.ruck@gmail.com>
2023-06-11 15:38:19 -0400

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -6,19 +6,19 @@
 //   };
 
 //Lewis' test database
-module.exports = {
-    HOST: "fsd08lewis.mysql.database.azure.com",
-    USER: "dbAdmin",
-    PASSWORD: "0W8r*7@3DHgA",
-    DB: "workrighttest" // do not make typos on database names, disaster
-  };
-  
 // module.exports = {
 //     HOST: "fsd08lewis.mysql.database.azure.com",
 //     USER: "dbAdmin",
 //     PASSWORD: "0W8r*7@3DHgA",
-//     DB: "authworkright" // do not make typos on database names, disaster
+//     DB: "workrighttest" // do not make typos on database names, disaster
 //   };
+  
+module.exports = {
+    HOST: "fsd08lewis.mysql.database.azure.com",
+    USER: "dbAdmin",
+    PASSWORD: "0W8r*7@3DHgA",
+    DB: "authworkright" // do not make typos on database names, disaster
+  };
 
 
 // HOST: fsd08workright.mysql.database.azure.com
